6093d5beeeb1bf42bade0dfc9fa6491887d8b82f
Elijah Dilzang Ayuba <elidondking.c@gmail.com>
2022-06-30 14:36:52 -0700

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/
- * main - Entry point
- * Return: Always 0 (sucess)
+/**
+ * main - print the strings that inputs the function
+ * Description:printing"Programming is like building a multilingual puzzle"
+ * Return:0
  */
 int main(void)
 {
