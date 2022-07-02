4919df1d8f52331c8330f63179811ca5c824df0a
Elijah Dilzang Ayuba <elidondking.c@gmail.com>
2022-07-02 02:30:51 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,29 @@
-#include <stdio.h> 
-  
- /** 
-  *main -> assign a random number to the variable n each time it is executed 
-  *and print the last digit of the number stored in the variable n 
-  *Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
- int ch; 
- int n; 
- for (ch = 48; ch <= 57; ch++) 
- { 
- for (n = 49; n <= 57; n++) 
- { 
- if (n > ch) 
- { 
- putchar(ch); 
- putchar(n); 
- if (ch != 56 || n != 57) 
- { 
- putchar(44); 
- putchar(32); 
- } 
- } 
- } 
- } 
-  
- putchar(10); /* this is an ascii code for new line*/ 
-  
- return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
